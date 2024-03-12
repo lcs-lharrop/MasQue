7e1a6434dd152e52c0f99b7c82d0330b90c3cb52
Lexi Harrop <lharrop@lcs.on.ca>
2024-03-12 12:54:31 -0600

    Initial Commit

---
+++ HiddenQuestions/HiddenQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiddenQuestionsApp.swift
+//  HiddenQuestions
+//
+//  Created by Lexi on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct HiddenQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
